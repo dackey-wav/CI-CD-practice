e70fe60c77e5e43c83515cb311d5987ff38af326
Danil Krassotov <danilkrasotov65@gmail.com>
2026-05-28 16:03:49 +0200

    Add nums convertion

--- utils.py
+++ utils.py
@@ -19,3 +19,13 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """Return the quotient of two numbers."""
     return a / b
+
+def to_binary(value: int) -> str:
+    """Convert a natural number from 0 to 100 into binary representation."""
+    if not isinstance(value, int):
+        raise TypeError("Value must be a natural number without decimal part.")
+
+    if value < 0 or value > 100:
+        raise ValueError("Value must be in range 0..100.")
+
+    return bin(value)[2:]
